c078c136624cfd63721eac37a206f3136a99174b
Art Poon <artpoon@gmail.com>
2024-04-30 18:40:27 -0400

    problems running R script on mac

--- scripts/topicspace.R
+++ scripts/topicspace.R
@@ -1,15 +1,16 @@
-require(wordspace)
 require(Matrix)
 require(irlba)
 
 setwd("~/git/tragula")
 
 # co-occurrence matrix
-ccm <- read.csv("test.csv", header=F)
+ccm <- read.csv("results/cooccur.csv", header=F)
+
+# presence/absence seems to work better
 sparse <- sparseMatrix(i=ccm[,1], j=ccm[,2], x=1, index1=FALSE)
 #sparse <- sparseMatrix(i=ccm[,1], j=ccm[,2], x=ccm[,3], index1=FALSE)
 
-index <- read.csv("index.txt")
+index <- read.csv("results/index.csv")
 index <- index[!is.na(index$index), ]
 
 p1 <- prcomp_irlba(t(sparse), n=6)
@@ -23,10 +24,3 @@ text(p1$x[,3:4], labels=index$word, cex=0.5)
 
 plot(p1$x[,5:6], type='n')
 text(p1$x[,5:6], labels=index$word, cex=0.5)
-
-#dm <- wordspace::dist.matrix(t(sparse), method="euclidean")
-#p2 <- prcomp(dm)
-#plot(p2$x)
-
-#require(uwot)
-#um <- umap(sparse)
