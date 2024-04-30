5f9433f88beb6fc9f2da64812a08528701450cd4
ArtPoon <artpoon@gmail.com>
2024-04-30 17:36:04 -0400

    switched to context-term matrix instead of term-term

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -63,7 +63,7 @@ def process(files, debug=False):
       - cooccur - dict, stores numbers of texts in which both words appear
     """
     by_author = {}
-    cooccur = {}
+    by_document = []
     all_words = {}
     for f in files:
         author = os.path.basename(f).split('.')[0]
@@ -90,35 +90,23 @@ def process(files, debug=False):
                 filtered.append(word)
 
             # increment word counts by author
+            for word in filtered:
+                if word not in all_words:
+                    all_words.update({word: 0})
+                all_words[word] += 1
+
             if debug:
-                for word in filtered:
-                    if word not in all_words:
-                        all_words.update({word: 0})
-                    all_words[word] += 1
-                continue  # skip next step
-            else:
-                for word in filtered:
-                    if word not in by_author[author]:
-                        by_author[author].update({word: 0})
-                    by_author[author][word] += 1
+                continue  # skip next steps
+
+            for word in filtered:
+                if word not in by_author[author]:
+                    by_author[author].update({word: 0})
+                by_author[author][word] += 1
 
             # record co-occurrences
-            intermed = list(set(filtered))
-            intermed.sort()  # w1 < w2
-            for i in range(len(intermed)-1):
-                w1 = intermed[i]
-                if w1 not in cooccur:
-                    cooccur.update({w1: {}})
-                for j in range(i+1, len(intermed)):
-                    w2 = intermed[j]
-                    if w2 not in cooccur[w1]:
-                        cooccur[w1].update({w2: 0})
-                    cooccur[w1][w2] += 1
+            by_document.append(dict([(w, filtered.count(w)) for w in set(filtered)]))
 
-    if debug:
-        return all_words, None
-    else:
-        return by_author, cooccur
+    return all_words, by_author, by_document
 
 
 if __name__ == "__main__":
@@ -128,39 +116,41 @@ if __name__ == "__main__":
                         help="Path to write by-author word counts (JSON)")
     parser.add_argument("--matrix", type=argparse.FileType('w'), required=False,
                         help="Path to write co-occurrence sparse matrix (CSV)")
-    parser.add_argument("--index", type=argparse.FileType('w'), required=False,
-                        help="Path to write indexed words (for interpreting CSV)")
+    parser.add_argument("--index", type=argparse.FileType('w'), required=True,
+                        help="Path to write indexed words for interpreting CSV output")
     args = parser.parse_args()
 
     files = glob(os.path.join(args.indir, "*.json"))
 
+    debug = False
     if args.counts is None or args.matrix is None:
-        counts, _ = process(files, debug=True)
-        intermed = [(count, word) for word, count in counts.items()]
-        intermed.sort(reverse=True)
-        for count, word in intermed:
-            print(f"{word},{count}")
-    else:
-        by_author, cooccur = process(files, debug=False)
-        json.dump(by_author, args.counts, indent=2)
+        debug = True
 
-        # collect all words
-        lex = set()
-        for w1, rows in cooccur.items():
-            lex.add(w1)
-            for w2, _ in rows.items():
-                lex.add(w2)
-        lex = sorted(lex)
-        index = dict([(w, i) for i, w in enumerate(lex)])
-        for w in lex:
-            args.index.write(f"{w}\n")
+    all_words, by_author, by_document = process(files, debug=debug)
 
-        # export co-occurrences as sparse matrix
-        writer = csv.writer(args.matrix)
-        for w1, rows in cooccur.items():
-            i = index[w1]
-            for w2, count in rows.items():
-                if count == 1:
-                    continue
-                j = index[w2]
+    # export indexed words
+    intermed = [(count, word) for word, count in all_words.items()]
+    intermed.sort(reverse=True)
+
+    writer = csv.writer(args.index)
+    writer.writerow(["word", "count", "index"])
+    index = {}
+    for idx, (count, word) in enumerate(intermed):
+        if count < 2:
+            continue
+        writer.writerow([word, count, idx])
+        index.update({word: idx})
+
+    if debug:
+        sys.exit()
+
+    # write counts by author
+    json.dump(by_author, args.counts, indent=2)
+
+    # export co-occurrences (context-term) as sparse matrix
+    writer = csv.writer(args.matrix)
+    for i, counts in enumerate(by_document):
+        for word, count in counts.items():
+            j = index.get(word, None)  # global indexing
+            if j is not None:
                 writer.writerow([i, j, count])
