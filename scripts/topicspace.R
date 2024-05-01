f05ae8d60c84c4c1c360e5940ce56caf0542cbf1
Art Poon <artpoon@gmail.com>
2024-04-30 21:00:40 -0400

    computed wasserstein

--- scripts/topicspace.R
+++ scripts/topicspace.R
@@ -1,5 +1,7 @@
 require(Matrix)
 require(irlba)
+require(transport)
+require(jsonlite)
 
 setwd("~/git/tragula")
 
@@ -13,14 +15,30 @@ sparse <- sparseMatrix(i=ccm[,1], j=ccm[,2], x=1, index1=FALSE)
 index <- read.csv("results/index.csv")
 index <- index[!is.na(index$index), ]
 
-p1 <- prcomp_irlba(t(sparse), n=6)
+p1 <- prcomp_irlba(t(sparse), n=8)
 plot(p1$x)
 
 plot(p1$x, type='n')
 text(p1$x, labels=index$word, cex=0.5)
-
 plot(p1$x[,3:4], type='n')
 text(p1$x[,3:4], labels=index$word, cex=0.5)
-
 plot(p1$x[,5:6], type='n')
 text(p1$x[,5:6], labels=index$word, cex=0.5)
+
+# load author-specific word counts
+by.author <- read_json("results/by_author.json", simplifyVector = TRUE)
+by.author <- sapply(by.author, unlist)  # more convenient named vectors
+
+idx <- match(index$word, names(by.author$poon_art))
+mass <- by.author$poon_art[idx]
+mass[is.na(mass)] <- 0.01
+mass <- mass / sum(mass)
+x <- wpp(p1$x, mass=mass)
+
+idx <- match(index$word, names(by.author$castellani_christina))
+mass <- by.author$castellani_christina[idx]
+mass[is.na(mass)] <- 0.01
+mass <- mass / sum(mass)
+y <- wpp(p1$x, mass=mass)
+
+wasserstein(x, y)
